4b65ae50b8b2b38c4e22124969569225913b54e9
David Goldwasser <david.goldwasser@nrel.gov>
2022-01-20 01:39:25 -0700

    Updated run with temp EP installer
    it addresses weather file parsing.

--- results/resources/common_info.rb
+++ results/resources/common_info.rb
@@ -7,15 +7,15 @@ module BestestResults
 
     if program == "OS"
       hash[:program_name_and_version] = "OpenStudio 3.3.0"
-      hash[:program_version_release_date] = "11/12/2021"
+      hash[:program_version_release_date] = "11/05/2021"
       hash[:program_name_short] = "OS"
     else
-      hash[:program_name_and_version] = "EnergyPlus 9.6.1"
-      hash[:program_version_release_date] = "11/12/2021"
+      hash[:program_name_and_version] = "EnergyPlus 9.6.0"
+      hash[:program_version_release_date] = "09/23/21"
       hash[:program_name_short] = "E+"
     end
 
-    hash[:results_submission_date] = "10/29/2021"
+    hash[:results_submission_date] = "01/20/2022"
     hash[:organization] = "National Renewable Energy Laboratory"
     hash[:organization_short] = "NREL"
 
