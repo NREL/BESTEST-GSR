24b642f16c0d0dd8aaef3389fd9437605b2d251a
David Goldwasser <david.goldwasser@nrel.gov>
2022-04-08 10:44:03 -0600

    no changes in results from E+ 9.6 to E+ 22.1
    I'm holding off on committing th Excel files (other than historical) until run on final release for submission.

--- results/resources/common_info.rb
+++ results/resources/common_info.rb
@@ -6,16 +6,16 @@ module BestestResults
     hash = {}
 
     if program == "OS"
-      hash[:program_name_and_version] = "OpenStudio 3.3.0"
-      hash[:program_version_release_date] = "11/05/2021"
+      hash[:program_name_and_version] = "OpenStudio 3.4.0"
+      hash[:program_version_release_date] = "??/??/2022"
       hash[:program_name_short] = "OS"
     else
-      hash[:program_name_and_version] = "EnergyPlus 9.6.0"
-      hash[:program_version_release_date] = "09/23/21"
+      hash[:program_name_and_version] = "EnergyPlus 22.1"
+      hash[:program_version_release_date] = "??/??/22"
       hash[:program_name_short] = "E+"
     end
 
-    hash[:results_submission_date] = "01/20/2022"
+    hash[:results_submission_date] = "??/??/2022"
     hash[:organization] = "National Renewable Energy Laboratory"
     hash[:organization_short] = "NREL"
 
