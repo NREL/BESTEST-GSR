c106ab1dcec6ec1059c722e04fef4dd4f3e3e176
David Goldwasser <david.goldwasser@nrel.gov>
2022-11-04 09:46:09 -0600

    Progress update with rc OS installer
    Few envelope values that aren't coming through, likely change in sql query in E+. Also few CE value changes to investigate.

--- results/resources/common_info.rb
+++ results/resources/common_info.rb
@@ -1,21 +1,21 @@
 module BestestResults
 
-  # define cases for section 5.2.3
+  # common data for spreadsheet headers
   def self.populate_common_info(program = "EP")
 
     hash = {}
 
     if program == "OS"
-      hash[:program_name_and_version] = "OpenStudio 3.4.0"
-      hash[:program_version_release_date] = "05/05/2022"
+      hash[:program_name_and_version] = "OpenStudio 3.5.0"
+      hash[:program_version_release_date] = "11/03/2022"
       hash[:program_name_short] = "OS"
     else
-      hash[:program_name_and_version] = "EnergyPlus 22.1"
-      hash[:program_version_release_date] = "03/29/22"
+      hash[:program_name_and_version] = "EnergyPlus 22.2"
+      hash[:program_version_release_date] = "09/27/22"
       hash[:program_name_short] = "E+"
     end
 
-    hash[:results_submission_date] = "05/27/2022"
+    hash[:results_submission_date] = "11/03/2022"
     hash[:organization] = "National Renewable Energy Laboratory"
     hash[:organization_short] = "NREL"
 
