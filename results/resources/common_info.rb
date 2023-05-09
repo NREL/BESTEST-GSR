094477b675fbcebcb4408020f60c06a517a331ae
David Goldwasser <david.goldwasser@nrel.gov>
2023-05-08 23:38:39 -0600

    360 run with rc
    Still have to address issues with values for table UNSHADED ANNUAL TRANSMITTED (Total Direct-Beam and Diffuse) and SHADED ANNUAL TRANSMITTED (Total Direct-Beam and Diffuse)

--- results/resources/common_info.rb
+++ results/resources/common_info.rb
@@ -6,16 +6,16 @@ module BestestResults
     hash = {}
 
     if program == "OS"
-      hash[:program_name_and_version] = "OpenStudio 3.5.0"
-      hash[:program_version_release_date] = "11/03/2022"
+      hash[:program_name_and_version] = "OpenStudio 3.6.0"
+      hash[:program_version_release_date] = "05/09/2023"
       hash[:program_name_short] = "OS"
     else
-      hash[:program_name_and_version] = "EnergyPlus 22.2"
-      hash[:program_version_release_date] = "09/27/22"
+      hash[:program_name_and_version] = "EnergyPlus 23.1"
+      hash[:program_version_release_date] = "03/28/23"
       hash[:program_name_short] = "E+"
     end
 
-    hash[:results_submission_date] = "11/03/2022"
+    hash[:results_submission_date] = "05/09/2023"
     hash[:organization] = "National Renewable Energy Laboratory"
     hash[:organization_short] = "NREL"
 
