030a7a16d2fd78da6f4dfa1af3661705155e781e
David Goldwasser <david.goldwasser@nrel.gov>
2024-11-15 10:07:36 -0700

    updating final results and release dates for E+ 24.2.1 and OS 3.9.0

--- results/resources/common_info.rb
+++ results/resources/common_info.rb
@@ -7,15 +7,15 @@ module BestestResults
 
     if program == "OS"
       hash[:program_name_and_version] = "OpenStudio 3.9.0"
-      hash[:program_version_release_date] = "11/15/2024 (expected)"
+      hash[:program_version_release_date] = "11/14/2024 (expected)"
       hash[:program_name_short] = "OS"
     else
-      hash[:program_name_and_version] = "EnergyPlus 24.2"
-      hash[:program_version_release_date] = "03/28/2024"
+      hash[:program_name_and_version] = "EnergyPlus 24.2.1"
+      hash[:program_version_release_date] = "10/03/24"
       hash[:program_name_short] = "E+"
     end
 
-    hash[:results_submission_date] = "11/06/2024"
+    hash[:results_submission_date] = "11/15/2024"
     hash[:organization] = "National Renewable Energy Laboratory"
     hash[:organization_short] = "NREL"
 
